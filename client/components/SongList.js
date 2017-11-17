8a9df6bb24e3a585dbe49b2e71b63f41e50aeade
Jason Gutierrez <jgutierrez@americanpayroll.org>
2017-11-17 01:36:53 -0600

    delete mutation and pointer on delete icon

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -3,13 +3,20 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import fetchSongs from '../queries/fetchSongs';
+import deleteSong from '../queries/deleteSong';
 
 class SongList extends Component {
+  onSongDelete(id) {
+    this.props.mutate({ variables: { id } })
+      .then(() => this.props.data.refetch());
+  }
+
   renderSongs(){
-    return this.props.data.songs.map(song => {
+    return this.props.data.songs.map(({ id, title }) => {
       return (
-        <li key={song.id} className="collection-item">
-          {song.title}
+        <li key={id} className="collection-item">
+          {title}
+          <i className="material-icons small right" onClick={() => this.onSongDelete(id)}>delete</i>
         </li>
       );
     });
@@ -33,4 +40,7 @@ class SongList extends Component {
   }
 }
 
-export default graphql(fetchSongs)(SongList);
+
+export default graphql(deleteSong)(
+  graphql(fetchSongs)(SongList)
+);

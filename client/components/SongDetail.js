3a8a0ae440ff2b86d3e2e90ee5c42e1743570faf
Jason Gutierrez <jgutierrez@americanpayroll.org>
2017-11-19 01:01:38 -0600

    <code>Project wrapup

---
+++ client/components/SongDetail.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import fetchSongByID from '../queries/fetchSongByID';
+import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
+
+class SongDetail extends Component {
+
+  render() {
+    const { song } = this.props.data;
+
+    if(!song) { return <div></div>;  }
+
+    return (
+      <div>
+        <Link to="/">Back</Link>
+        <h3>{song.title}</h3>
+        <LyricList lyrics={song.lyrics} />
+        <LyricCreate songId={this.props.params.id}/>
+      </div>
+    );
+  }
+} 
+
+
+
+export default graphql(fetchSongByID, {
+  options: (props) => { return { variables: { id: props.params.id } } }
+})(SongDetail);
---
+++ client/queries/addNewLyric.js
@@ -0,0 +1,9 @@
+import gql from 'graphql-tag';
+
+export default gql`
+mutation AddLyricToSong($content:String, $songId: ID) {
+  addLyricToSong(content:$content, songId:$songId) {
+    id lyrics { id content likes }
+  }
+}
+`;
